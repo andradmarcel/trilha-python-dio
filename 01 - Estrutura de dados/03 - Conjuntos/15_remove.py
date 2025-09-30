ad1f72694a67e1352d04c218dc97f2f537611f5c
Marcel Andrade <luismarcel402@gmail.com>
2025-09-30 16:18:14 -0300

    comentário no arquivo pop

--- 01 - Estrutura de dados/03 - Conjuntos/15_remove.py
+++ 01 - Estrutura de dados/03 - Conjuntos/15_remove.py
@@ -3,3 +3,6 @@ numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
 print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 print(numeros.remove(0))  # 0
 print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+#remove da erro com elemento que nao existe
+#caso voce queira remover(escolher entre discard e remove depende da necessidade)
