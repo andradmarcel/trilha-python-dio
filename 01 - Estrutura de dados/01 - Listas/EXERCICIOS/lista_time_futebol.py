8e0efc2b574637af1b2774786281cc84dc34169a
Marcel Andrade <luismarcel402@gmail.com>
2025-09-30 12:40:31 -0300

    exercicio 3 sobre listas

---
+++ 01 - Estrutura de dados/01 - Listas/EXERCICIOS/lista_time_futebol.py
@@ -0,0 +1,12 @@
+
+
+
+jogadores = ["Neymar", "Vinícius Jr", "Alisson", "Casemiro", "Marquinhos", "Thiago Silva", "Paquetá", "Richarlison"]
+jogadores_ordenados = sorted(jogadores)
+titulares = jogadores_ordenados[:5]
+reservas = jogadores_ordenados[5:]
+print("\n\n\n")
+print(jogadores)
+print(jogadores_ordenados)
+print(titulares)
+print(reservas)
