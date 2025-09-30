b0d54306214b4426a24ec55c494de7e1ceb3dedb
Marcel Andrade <luismarcel402@gmail.com>
2025-09-30 18:39:45 -0300

    adcionei um for apra melhor visualizacao

--- 01 - Estrutura de dados/04 - Dicionários/16_del.py
+++ 01 - Estrutura de dados/04 - Dicionários/16_del.py
@@ -9,4 +9,7 @@ del contatos["guilherme@gmail.com"]["telefone"]
 del contatos["chappie@gmail.com"]
 
 # {'guilherme@gmail.com': {'nome': 'Guilherme'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}  # noqa
-print(contatos)
+
+
+for chave, valor in contatos.items():
+    print(chave, valor)
