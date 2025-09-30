e0adfe6061a02add5672a988340c9ae096bcde5a
Marcel Andrade <luismarcel402@gmail.com>
2025-09-30 15:59:18 -0300

    testanto uma funcao para iterar sobre o set

--- 01 - Estrutura de dados/03 - Conjuntos/02_iterar_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/02_iterar_conjuntos.py
@@ -5,3 +5,12 @@ for carro in carros:
 
 for indice, carro in enumerate(carros):
     print(f"{indice}: {carro}")
+
+
+def iterar_set(conjunto):
+    for i in conjunto:
+        print(i)
+    
+    
+iterar_set(carros) 
+    
