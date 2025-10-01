8249e18e781fd9601d4e3eeff8aaafc13fc3e075
Marcel Andrade <luismarcel402@gmail.com>
2025-10-01 17:02:13 -0300

    testando outros metodos de usar funcoes

--- 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
+++ 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
@@ -5,9 +5,21 @@ def calcular_total(numeros):
 def retorna_antecessor_e_sucessor(numero):
     antecessor = numero - 1
     sucessor = numero + 1
-
     return antecessor, sucessor
 
+# def analisar_numero(numero):
+#     antecessor = numero - 1
+#     sucessor = numero + 1
+#     print(f"Analisando o número {numero}: seu antecessor é {antecessor} e seu sucessor é {sucessor}.")
+    
+def analisar_numero(numero):
+    # Aqui está a mágica: chamamos a primeira função
+    # e desempacotamos o resultado (a tupla) em duas variáveis.
+    antecessor, sucessor = retorna_antecessor_e_sucessor(numero)
+
+    # Agora a função se preocupa apenas em exibir os dados.
+    print(f"Analisando o número {numero}: seu antecessor é {antecessor} e seu sucessor é {sucessor}.")
 
 print(calcular_total([10, 20, 34]))  # 64
 print(retorna_antecessor_e_sucessor(10))  # (9, 11)
+analisar_numero(10)
