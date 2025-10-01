8b000bd570dd0f045c41d431d4e8be8eb0c395e0
Marcel Andrade <luismarcel402@gmail.com>
2025-10-01 19:22:44 -0300

    melhor pratica para usar função

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -8,3 +8,17 @@ def salario_bonus(bonus):
 
 
 salario_bonus(500)  # 2500
+
+
+# A função agora recebe o salário atual como um argumento
+# e não modifica mais a variável global. Ela é mais previsível.
+def calcular_salario_com_bonus(salario_atual, bonus):
+    return salario_atual + bonus
+
+
+# Para ver o resultado, precisamos usar o print()
+novo_salario = calcular_salario_com_bonus(salario, 500)
+
+print(f"O salário original era: R$ {salario}")
+print(f"O novo salário com bônus é: R$ {novo_salario}")
+
