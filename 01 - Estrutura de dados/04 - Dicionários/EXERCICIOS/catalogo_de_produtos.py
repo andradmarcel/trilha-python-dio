9d30f2c1f91a65c0bd85c53e030efa3869703278
Marcel Andrade <luismarcel402@gmail.com>
2025-10-01 10:57:34 -0300

    update primeiro exercicio, tornando o codigo mais robusto

--- 01 - Estrutura de dados/04 - Dicionários/EXERCICIOS/catalogo_de_produtos.py
+++ 01 - Estrutura de dados/04 - Dicionários/EXERCICIOS/catalogo_de_produtos.py
@@ -1,7 +1,7 @@
 
 def iterar_dicionario(dicionario):
     for chave, valor in dicionario.items():
-        print(chave, valor)
+        print(chave.title(), f"R${valor:.2f}")
 
 
 
@@ -10,11 +10,17 @@ catalogo = {"caderno": 15.50, "caneta": 3.00, "camisa": 70.80, "chinelo": 42.90,
 
 
 while True:
-    nome_produto = input("Digite o nome de um produto: ")
+    
+    nome_produto = input("Digite o nome de um produto: ").lower().strip()
     if nome_produto in catalogo:
         print(f"O preço do(a) '{nome_produto}' é de R${catalogo[nome_produto]:.2f}")
+        sair = input("Deseja sair do programa? [s/n]").lower().strip()
     else: 
         preco_produto = float(input(f"Produto '{nome_produto}' nao encontrado, digite seu preço para aciona-lo ao catalogo: "))
         catalogo[nome_produto] = preco_produto
         iterar_dicionario(catalogo)
-        
+        sair = input("Deseja sair do programa? [s/n] ").lower().strip()
+    if sair == "s" or sair == "sim":
+        break
+    else:
+        continue    
