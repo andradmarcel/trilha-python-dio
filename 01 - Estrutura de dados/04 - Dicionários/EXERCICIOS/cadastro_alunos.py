c00e13b253afa656bc3e198af70318a905421be4
Marcel Andrade <luismarcel402@gmail.com>
2025-10-01 13:37:16 -0300

    ultimo exercício proposto pelo Gemini de dicionários, senti bastante dificuldade mas fiz sozinho, a ultima parte de adicionar nova materia e nota esta errado. irei corrigir junto com o Gemini

---
+++ 01 - Estrutura de dados/04 - Dicionários/EXERCICIOS/cadastro_alunos.py
@@ -0,0 +1,58 @@
+
+
+alunos = {
+    "100": {
+        "nome": "marcel",
+        "curso": "engenharia",
+        "notas": {
+            "matematica": 9,
+            "programacao": 10,
+            "computacao": 8.5
+        }
+    },
+    "101": {
+        "nome": "mariana",
+        "curso": "engenharia",
+        "notas": {
+            "matematica": 10,
+            "programacao": 8,
+            "nuvem": 9.5
+        }
+    }
+}
+
+
+print(10*"=","Consulta Aluno",10*"=")
+while True: 
+    try:
+        id_aluno = input("Digite o ID do aluno para consulta e 'sair' para fechar o programa: ").lower().strip()
+        if id_aluno == "sair":
+            print("Encerrando consulta")
+            break
+        
+        if id_aluno in alunos:
+            nome = alunos[id_aluno]["nome"]
+            curso = alunos[id_aluno]["curso"]
+            notas = alunos[id_aluno]["notas"]
+
+            print(f"O aluno '{nome}'".title(),f"esta cursando '{curso}'")
+            print(f"Notas do aluno {nome.title()}:")
+        
+            for materia, nota in notas.items():
+                print(f"{materia.title()} nota: {nota:.2f}")
+            sim_ou_nao = input(f"Gostaria de adicionar uma nova materia e nota para o aluno(a) {nome.title()} [s/n]: ").lower().strip()
+            if sim_ou_nao == "s" or sim_ou_nao == "sim":
+                nova_materia = input("Digite a materia: ").lower().strip()
+                nova_nota = int(input("Digite a nota: "))
+                notas.update(nova_materia, nova_nota)
+                for materia, nota in notas.items():
+                    print(f"{materia.title()} nota: {nota:.2f}")
+            else:
+                print("voltando")
+                
+                
+                
+        else: 
+            print("Aluno nao encontrado")
+    except ValueError:
+        print("Erro inesperado, tente novamente")
